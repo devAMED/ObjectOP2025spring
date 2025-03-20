5dd339161dc66c4eec7f940ad297061ca3c78d32
devAMED <syntaggo@gmail.com>
2025-03-20 11:22:28 +0400

    Add files via upload
    fixed few errors in the code

---
+++ Assignment01.java
@@ -0,0 +1,16 @@
+package workshop.w1.muhammad_ahmed_1;
+
+
+/**
+ * Correct formatting, naming conventions and the code itself
+ * 1 point
+ */
+class Assignment01 {
+    public static void main(String[] args) {
+
+        int a = 17;
+        int b = 18;
+
+        System.out.println("The sum of the numbers is" + (a + b));
+    }
+}
